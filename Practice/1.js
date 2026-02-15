26b17cf3edf106713200e1926f857cb487617dd9
Bhavya Anand <bhavya.anand.2101@gmail.com>
2026-02-15 09:48:42 +0530

    Progress update

---
+++ Practice/1.js
@@ -0,0 +1,34 @@
+// const hoverBox = document.getElementById('hover-box');
+
+// hoverBox.addEventListener('mouseover', () => {
+//     hoverBox.style.backgroundColor = 'blue';
+// });
+
+// hoverBox.addEventListener('mouseout', () => {
+//     hoverBox.style.backgroundColor = 'red';
+// });
+
+// keyboard Event keydown
+// document.addEventListener('keydown', function(event){
+//     alert("Key Pressed: "+event.key);
+// })
+
+// keyboard Event keup
+// document.addEventListener('keyup', function(event){
+//     alert("Key Pressed: "+event.key);
+// })
+
+// Coding Q
+// const keyInput = document.getElementById('key-input');
+// keyInput.addEventListener('keydown', event => {
+//     console.log(`key pressed: ${event.key}`);
+// });
+
+// form
+// const form= document.getElementById('my form');
+// form.addEventListener('submit',function(event){
+//     event.preventDefault();
+//     alert("Form submitted");
+// });
+
+// advanced forms 
