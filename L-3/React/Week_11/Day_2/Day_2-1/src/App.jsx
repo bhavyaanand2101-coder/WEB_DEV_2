f2a24832cfb16a5adc5eb93e9a431ef76dc87a56
bhavyaanand2101-coder <bhavya.anand.2101@gmail.com>
2026-04-02 22:25:37 +0530

    Progress update

--- L-3/React/Week_11/Day_2/Day_2-1/src/App.jsx
+++ L-3/React/Week_11/Day_2/Day_2-1/src/App.jsx
@@ -1,8 +1,22 @@
-// 
-// 
-// 
-// 
-// 
-// 
-/
-// /
+import Avt from "./Components/Avt"
+import Avatar from "./Components/Avatar1"
+function App(){
+    let arr=["Task 1", "Task 2", "Task 3", "Task 4", "Task 5", "Task 6"]
+
+    const ans= arr.map()
+    return <div>
+    <ul>
+        <li>Task 1</li>
+        <li>Task 2</li>
+        <li>Task 3</li>
+        <li>Task 4</li>
+        <li>Task 5</li>
+        <li>Task 6</li>
+    </ul>
+    <Avt name="John Doe" age={30} />
+     <h1>Hello World</h1>
+    <Avatar />
+    </div>
+}
+
+export default App;
