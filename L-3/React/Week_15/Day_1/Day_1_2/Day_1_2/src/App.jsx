acf53617654be34f781fc9aaec67175e4feae04a
bhavyaanand2101-coder <bhavya.anand.2101@gmail.com>
2026-04-20 21:01:34 +0530

    Progress update

---
+++ L-3/React/Week_15/Day_1/Day_1_2/Day_1_2/src/App.jsx
@@ -0,0 +1,12 @@
+import Nav from "./Components/Nav";
+import Main from "./Components/Main";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to Context Api</h1>
+      <Nav />
+      <Main />
+    </div>
+  );
+}
