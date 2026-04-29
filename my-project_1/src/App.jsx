df50c333832404b8285f5ce5a80884062c823152
bhavyaanand2101-coder <bhavya.anand.2101@gmail.com>
2026-04-29 21:22:40 +0530

    Progress update

--- my-project_1/src/App.jsx
+++ my-project_1/src/App.jsx
@@ -205,10 +205,17 @@
 //     </div>
 //   );
 // }
+import React from 'react';
+import axios from 'axios';
 
-// 
-// 
-// 
-// 
-// 
-// 
+export default async function App() {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>{response.data.title}</p>
+    </div>
+  );
+}   
+console.log("App component rendered");
